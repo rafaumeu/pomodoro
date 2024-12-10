e8c73cd23582d2a52fe4e55f6d91e03126f56238
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-10 16:52:31 -0300

    typescript do formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,11 +19,16 @@ const newCycleFormValidationSchema = zod.object({
     .min(5, 'O ciclo precisa ser de de no mínimo 5 minutos')
     .max(60, 'O ciclo precisa ser de de no máximo 60 minutos'),
 })
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
   const task = watch('task')
