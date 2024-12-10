7064331330e5cb585e557337729ea5416d1505d3
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-10 14:44:07 -0300

    melhorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,14 +17,24 @@ export function Home() {
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
+            list="task-suggestions"
             type="text"
             placeholder="Dê um nome para o seu projeto"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"  />
+            <option value="Projeto 2"  />
+            <option value="Projeto 3"  />
+
+          </datalist>
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos.</span>
         </FormContainer>
