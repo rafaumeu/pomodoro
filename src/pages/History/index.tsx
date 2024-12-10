382ec9e2a7302781018180a751d179aa7e5b9fb4
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-10 16:42:29 -0300

    validando formulários

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -19,31 +19,41 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td><Status statusColor='green'>Concluído</Status></td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td><Status statusColor='green'>Concluído</Status></td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td><Status statusColor='yellow'>Em andamento</Status></td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td><Status statusColor='red'>Interrompido</Status></td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td><Status statusColor='green'>Concluído</Status></td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
           </tbody>
         </table>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import * as zod from 'zod'
 import {
   CountDownContainer,
   FormContainer,
@@ -10,9 +12,17 @@ import {
   TaskInput,
 } from './styles'
 
-
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de de no máximo 60 minutos'),
+})
 export function Home() {
-  const { register, handleSubmit, watch} = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
   function handleCreateNewCycle(data: any) {
     console.log(data)
   }
