18f65a92152a6bb0387910c93add65b112da6119
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-10 15:01:02 -0300

    component history estrutura e estilos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,10 +22,9 @@ export function Home() {
             placeholder="Dê um nome para o seu projeto"
           />
           <datalist id="task-suggestions">
-            <option value="Projeto 1"  />
-            <option value="Projeto 2"  />
-            <option value="Projeto 3"  />
-
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
           </datalist>
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
