3e0554a73832f055ecd39f2e6951e57f2b82022c
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-10 11:45:46 -0300

    cores e fonte

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 interface ButtonContainerProps {
   variant: ButtonVariant
@@ -16,11 +16,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background-color: ${(props) => props.theme.primary};
+  background-color: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme.white};
-  /* ${(props) => {
-    return css`
-      background-color: ${buttonVariants[props.variant]};
-    `
-  }} */
 `
